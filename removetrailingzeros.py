864dc7e02197b4c59c2312de028f6a376f387535
soham <kshom248@gmail.com>
2024-02-28 22:30:49 +0530

    28-02-24

---
+++ removetrailingzeros.py
@@ -0,0 +1,21 @@
+# num="51230100"
+# l=[]
+# for i in num:
+#     l.append(i)
+# print(l)
+    
+# for i in range(len(l)):
+#     if l[-1]=="0":
+#         l.remove(l[-1])
+# print(l)
+
+# str=''.join(l[::1])
+# print(str)
+
+class Solution:
+    def removeTrailingZeros(self, num: str) -> str:
+        number = int(num)
+        while number%10 == 0:
+            number = number//10
+        num = ''.join(str(number))
+        return num
