e3e6ec4311baff91a70a09a9ff8a9f9aacd22a2e
soham <kshom248@gmail.com>
2024-03-04 00:04:58 +0530

    04.03.24

---
+++ detectCapital.py
@@ -0,0 +1,19 @@
+def detectCapital(word)->bool:
+    allCaps = True
+    for i in word:
+        if ord(i)<=90:
+            allCaps = True
+        else:
+            allCaps = False
+            break
+        
+    if allCaps == True:
+        return True
+    
+    for i in range(1,len(word)):
+        if ord(word[i])<97:
+            return False
+        
+    return True 
+
+print(detectCapital("elo"))
