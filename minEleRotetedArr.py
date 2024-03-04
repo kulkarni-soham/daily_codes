15582e6835bc119162a5413da376cb8abdd21350
soham <sohamprakashkulkarni@gmail.com>
2024-03-04 22:32:57 +0530

    04.03.24

---
+++ minEleRotetedArr.py
@@ -0,0 +1,30 @@
+def findMin(nums) -> int:
+    left=0
+    right=len(nums)-1
+    minn=nums[0]
+    
+    if nums[0]<=nums[right]:
+        return nums[0]
+    
+    while True:
+        if left>=right:
+            return minn
+    
+        mid=(left+right)//2
+    
+        if(nums[mid]<minn):
+            minn=nums[mid]
+    
+        if nums[mid+1]<nums[mid]:
+            return nums[mid+1]
+    
+        if (nums[mid-1]>nums[mid]):
+            return nums[mid]
+    
+        if nums[mid]>nums[right]:
+            left=mid+1
+    
+        if nums[mid]<nums[right]:
+            right=mid-1
+            
+print(findMin([3,4,5,1,2]))
