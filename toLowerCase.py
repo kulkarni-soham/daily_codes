e6f12e097555d68ff835e6e343dab40bda92269e
soham <sohamprakashkulkarni@gmail.com>
2024-03-05 22:11:33 +0530

    05.03.24

---
+++ toLowerCase.py
@@ -0,0 +1,10 @@
+def toLowerCase( s) :
+    low = ""
+    for i in s:
+        if i.isupper():
+            low += i.lower()
+        else:
+            low+=i
+    return low
+
+print(toLowerCase("HelloPython"))
