e8bb7d2d55dc8048ea91176d0947637f5eb9cb0a
soham <sohamprakashkulkarni@gmail.com>
2024-03-13 23:34:09 +0530

    13.04.24

--- singleNumber.py
+++ singleNumber.py
@@ -1,8 +1,15 @@
 def singleOccur(nums):
+    d = {}
+    res = 0
     for i in nums:
-        if(nums.count(i)==1):
-            return i
-        
-    return -1
-
-print(singleOccur([1,0,0, 1,2,3,4,3,3,2]))
+        if (i not in d):
+            d[i] = 1
+        else:
+            d[i] += 1
+            
+    for k,v in d.items():
+        if (v == 1):
+            res = k
+            
+    return res
+print(singleOccur([1,0,0,1,2,3,4,3,3,2]))
