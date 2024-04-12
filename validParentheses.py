192f1543acbaca33f6179aedfdc7a6b789002ed3
soham <sohamprakashkulkarni@gmail.com>
2024-04-12 23:32:19 +0530

    12.04.24

---
+++ validParentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if s[0] in (')', '}', ']'):
+            return False
+
+        stack = []
+
+        par = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        for paran in s:
+            if paran in ('(', '[', '{'): # 1
+                stack.append(paran)
+
+            elif stack and par[paran] == stack[-1]: # 2
+                stack.pop()
+
+            else:
+                return False # 3
+
+        return len(stack) == 0
