1fef705f7836ab971df53fc4a461e58432bfc396
soham <sohamprakashkulkarni@gmail.com>
2024-04-20 18:01:38 +0530

    20.04.24

---
+++ buildArray.java
@@ -0,0 +1,12 @@
+class buildArray {
+    public static void main(String[] args) {
+        int [] nums= {0,5,1,2,3,4};
+        int [] ans = new int[nums.length];
+        for(int i=0; i< ans.length; i++){
+            ans[i] = nums[nums[i]];
+        }
+        for(int i=0; i<ans.length; i++){
+            System.out.println(ans[i]);
+        }
+    }
+}
