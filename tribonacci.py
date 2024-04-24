c6b218948d2e67d810788cf8ffe82db93861225a
soham <sohamprakashkulkarni@gmail.com>
2024-04-24 23:53:19 +0530

    24.04.24

---
+++ tribonacci.py
@@ -0,0 +1,18 @@
+def tribonacci(n):
+    a = 0
+    b = 1
+    c = 1
+    result = 0
+    if n == 0:
+        return 0
+    elif n < 3 :
+        return 1
+    for i in range(n-2):
+        result = a + b + c
+        a = b
+        b = c
+        c = result
+        
+    return result
+
+print(tribonacci(3))
